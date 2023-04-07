06757df90b5b12448fa0b59177e5840488060884
buthiattila <buthi.attila@gmail.com>
2023-04-07 09:33:09 +0200

    material felzárkózás

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './feature/home/home.component';
-import { LoginComponent } from './feature/login/login.component';
-import { PostsComponent } from './feature/posts/posts.component';
-import { PostComponent } from './feature/post/post.component';
-import { InfoComponent } from './info/info.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './feature/home/home.component';
+import {LoginComponent} from './feature/login/login.component';
+import {PostsComponent} from './feature/posts/posts.component';
+import {PostComponent} from './feature/post/post.component';
+import {InfoComponent} from './info/info.component';
 
 @NgModule({
   declarations: [
@@ -32,4 +32,5 @@ import { InfoComponent } from './info/info.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
